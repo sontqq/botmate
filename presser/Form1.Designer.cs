3159c873674e98f73243df242772cf8ae5017c5c
Eged Zsolt <sont16@gmail.com>
2019-03-29 13:06:03 +0100

    init

---
+++ presser/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace presser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 78);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 89);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 169);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(167, 89);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(186, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Cycle: 0";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(186, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(108, 58);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Rename Window";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(168, 26);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "World of Warcraft";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 44);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(168, 26);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "BOT";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 280);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(463, 26);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "+12+E+3Q+R+4T-G0RX8";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 319);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "\'+\' = wait(500ms)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 257);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Spell queue:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(142, 319);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "\'-\' = tab";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(186, 98);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Elapsed: 0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(296, 188);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(106, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "[spellqueue]";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(186, 168);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(108, 20);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Actual button:";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(300, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 58);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Not yet";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label8.Location = new System.Drawing.Point(186, 238);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(51, 20);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "x: y: z:";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(382, 13);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(93, 24);
+            this.checkBox1.TabIndex = 15;
+            this.checkBox1.Text = "Rotate?";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(382, 43);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(76, 24);
+            this.checkBox2.TabIndex = 16;
+            this.checkBox2.Text = "Loot?";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 359);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "BotMate 0.1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ presser/Form1.cs
@@ -0,0 +1,436 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+//using System.Windows.Forms.Timer;
+using System.Windows.Forms;
+using WindowsInput;
+using System.Timers;
+//using Magic;
+using System.Security.Principal;
+
+
+namespace presser
+{
+    /*public static*/
+
+    public partial class Form1 : Form
+    {
+        // [DllImport("user32.dll")]
+        // static extern IntPtr FindWindow(string windowClass, string windowName);
+
+        [DllImport("user32.dll")]
+        static extern short VkKeyScan(char ch);
+
+        [DllImport("user32.dll")]
+        static extern int MapVirtualKey(int uCode, uint uMapType);
+
+        [DllImport("user32.dll")]
+        static extern bool SetWindowText(IntPtr hWnd, string text);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll")]
+        static extern bool PostMessage(IntPtr hWnd, IntPtr Msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        static extern bool PostS(IntPtr hWnd, String Msg, int wParam, int lParam);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessageW(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("User32.dll")]
+        public static extern void ReleaseDC(IntPtr dc);
+
+        [DllImport("User32.dll")]
+        public static extern IntPtr GetDC(IntPtr hwnd);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+
+        private const int APPCOMMAND_VOLUME_MUTE = 0x80000;
+        private const int WM_APPCOMMAND = 0x319;
+
+
+        //System.Timers.Timer aTimer = new System.Timers.Timer();
+        //System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+
+        System.Timers.Timer aTimer = new System.Timers.Timer();
+        System.Windows.Forms.Timer timer;
+
+        Stopwatch stopwatch = new Stopwatch();
+        
+        public uint counter = 0;
+        public string actual;
+
+        public static string windowName = "BOT";
+        public static bool forogjon = true;
+
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+        const int WM_SYSKEYDOWN = 0x0104;
+        const int WM_SYSKEYUP = 0x0105;
+        const int WM_KEYDOWN = 0x100;
+        const int WM_KEYUP = 0x101;
+        const int WM_CHAR = 0x102;
+        const int WM_SYSCHAR = 0x106;
+        const int WM_IME_CHAR = 0x286;
+
+        const int W = 0x57;
+        const int A = 0x41;
+        const int S = 0x53;
+        const int D = 0x44;
+        const int TAB = 0x09;
+        const int E = 0x45;
+
+        const int Q = 0x51;
+        const int R = 0x52;
+        const int X = 0x58;
+        const int C = 0x43;
+        const int G = 0x47;
+
+        const int one = 0x31;
+        const int two = 0x32;
+        const int three = 0x33;
+        const int four = 0x34;
+        const int five = 0x35;
+        const int six = 0x36;
+        const int seven = 0x37;
+        const int eight = 0x38;
+        const int nine = 0x39;
+        const int zero = 0x30;
+    
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int ConvertX(char character) {
+            switch (character)
+            {
+                case 'a':
+                    return 0x41;
+                case 'b':
+                    return 0x42;
+                case 'c':
+                    return 0x43;
+                case 'd':
+                    return 0x44;
+                case 'e':
+                    return 0x45;
+                case 'f':
+                    return 0x46;
+                case 'g':
+                    return 0x47;
+                case 'h':
+                    return 0x48;
+                case 'i':
+                    return 0x49;
+                case 'j':
+                    return 0x4A;
+                case 'k':
+                    return 0x4B;
+                case 'l':
+                    return 0x4C;
+                case 'm':
+                    return 0x4D;
+                case 'n':
+                    return 0x4E;
+                case 'o':
+                    return 0x4F;
+                case 'p':
+                    return 0x50;
+                case 'q':
+                    return 0x51;
+                case 'r':
+                    return 0x52;
+                case 's':
+                    return 0x53;
+                case 't':
+                    return 0x54;
+                case 'u':
+                    return 0x55;
+                case 'v':
+                    return 0x56;
+                case 'w':
+                    return 0x57;
+                case 'x':
+                    return 0x58;
+                case 'y':
+                    return 0x59;
+                case 'z':
+                    return 0x5A;
+                case '1':
+                    return 0x31;
+                case '2':
+                    return 0x32;
+                case '3':
+                    return 0x33;
+                case '4':
+                    return 0x34;
+                case '5':
+                    return 0x35;
+                case '6':
+                    return 0x36;
+                case '7':
+                    return 0x37;
+                case '8':
+                    return 0x38;
+                case '9':
+                    return 0x39;
+                case '0':
+                    return 0x30;
+                default:
+                    return 0x20; // SPACE
+            }
+        }
+
+        public void DoMouseClick()
+        {
+            /*
+            uint X = (uint)Cursor.Position.X;
+            uint Y = (uint)Cursor.Position.Y;
+            */
+            
+            mouse_event(MOUSEEVENTF_RIGHTDOWN | MOUSEEVENTF_RIGHTUP, 555, 339, 0, 0);
+            mouse_event(MOUSEEVENTF_RIGHTDOWN | MOUSEEVENTF_RIGHTUP, 548, 437, 0, 0);
+            mouse_event(MOUSEEVENTF_RIGHTDOWN | MOUSEEVENTF_RIGHTUP, 665, 355, 0, 0);
+            mouse_event(MOUSEEVENTF_RIGHTDOWN | MOUSEEVENTF_RIGHTUP, 694, 443, 0, 0);
+            mouse_event(MOUSEEVENTF_RIGHTDOWN | MOUSEEVENTF_RIGHTUP, 637, 382, 0, 0);
+        }
+
+        public void DoLoopMouse()
+        {
+            const int row = 30;
+            const int column = 10;
+            int[,] array = new int[row, column];
+
+            for (int i = 504; i < 720; i++)
+            {
+                for (int j = 288; j < 504; j++)
+                {
+
+                }
+            }
+        }
+
+        void OnTimedEvent(object source, ElapsedEventArgs e){
+            counter++;
+            IntPtr WindowToFind = FindWindow(null, textBox2.Text);
+            forog();
+            /*
+            BlackMagic wow = new BlackMagic();
+            try
+            {
+                IntPtr basewow = wow.MainModule.BaseAddress;
+                wow.OpenProcessAndThread(SProcess.GetProcessFromWindowTitle("BOT"));
+                float playerx = wow.ReadFloat((uint)basewow + 0x74);
+                float playery = wow.ReadFloat((uint)basewow + 0x78);
+                float playerz = wow.ReadFloat((uint)basewow + 0x7C);
+                label8.Text = "x: " + playerx.ToString() + " y: " + playery.ToString() + " z: " + playerz.ToString();
+            }
+            catch (Exception ex) {
+                label8.Text = ex.Message;
+            }
+            */
+            foreach (var g in textBox3.Text)
+            {
+                // / jelre egyeb
+                char KEY = g;
+                actual = KEY.ToString();
+                if (g == '+')
+                {
+                    Thread.Sleep(500);
+                }
+                if (g == '-')
+                {
+                    PostMessage(WindowToFind, (IntPtr)WM_SYSKEYDOWN, (IntPtr)TAB, IntPtr.Zero);
+                    PostMessage(WindowToFind, (IntPtr)WM_SYSKEYUP, (IntPtr)TAB, IntPtr.Zero);
+                }
+                else
+                {
+                    PostMessage(WindowToFind, (IntPtr)WM_SYSKEYDOWN, (IntPtr)KEY, IntPtr.Zero);
+                    PostMessage(WindowToFind, (IntPtr)WM_SYSKEYUP, (IntPtr)KEY, IntPtr.Zero);
+                    // DEFAULT WAIT
+                    Thread.Sleep(300);
+                }
+            }
+            Thread.Sleep(300);
+            if (checkBox2.Checked == true)
+            {
+                DoMouseClick();
+            }
+        }
+        /*
+        public void hack() {
+            // CTM 0xD0F390
+            BlackMagic wow = new BlackMagic();
+            IntPtr WindowToFind = FindWindow(null, windowName);
+            try
+            {
+                IntPtr basewow = wow.MainModule.BaseAddress;
+                wow.OpenProcessAndThread(SProcess.GetProcessFromWindowTitle("BOT"));
+                var name = wow.ReadASCIIString((uint)basewow + 0xEC4668, 256);
+                var islooting = wow.ReadByte((uint)basewow + 0xDD3D44);
+            
+                var asd = wow.ReadASCIIString((uint)basewow + 0xDD3D44, 256);
+                var x = wow.ReadASCIIString((uint)basewow + 0xEC47F1, 256);
+                var y = wow.ReadASCIIString((uint)basewow + 0xD0F390, 256);
+                var z = wow.ReadASCIIString((uint)basewow + 0x1274FE4, 256);
+            
+                float playerx = wow.ReadFloat((uint)basewow + 0x74);
+                float playery = wow.ReadFloat((uint)basewow + 0x78);
+                float playerz = wow.ReadFloat((uint)basewow + 0x7C);
+                MessageBox.Show(z.ToString());
+            }
+            catch (Exception ex) { }
+        }*/
+
+        public void draw()
+        {
+
+        }
+
+        public void pull() {
+            actual = "PULL";
+            IntPtr WindowToFind = FindWindow(null, windowName);
+            PostMessage(WindowToFind, (IntPtr)WM_SYSKEYDOWN, (IntPtr)TAB, IntPtr.Zero);
+            PostMessage(WindowToFind, (IntPtr)WM_SYSKEYUP, (IntPtr)TAB, IntPtr.Zero);
+            PostMessage(WindowToFind, (IntPtr)WM_SYSKEYDOWN, (IntPtr)X, IntPtr.Zero);
+            PostMessage(WindowToFind, (IntPtr)WM_SYSKEYUP, (IntPtr)X, IntPtr.Zero);
+        }
+        public void forog() {
+            if (checkBox1.Checked == true)
+            {
+                if (checkBox2.Checked == true)
+                {
+                    DoMouseClick();
+                }
+                // BALRA 90 - JOBBRA 180
+                actual = "FOROG";
+                IntPtr WindowToFind = FindWindow(null, windowName);
+                PostMessage(WindowToFind, (IntPtr)WM_SYSKEYDOWN, (IntPtr)A, IntPtr.Zero);
+                Thread.Sleep(500);
+                PostMessage(WindowToFind, (IntPtr)WM_SYSKEYUP, (IntPtr)A, IntPtr.Zero);
+                //pull();
+                PostMessage(WindowToFind, (IntPtr)WM_SYSKEYDOWN, (IntPtr)D, IntPtr.Zero);
+                Thread.Sleep(800);
+                PostMessage(WindowToFind, (IntPtr)WM_SYSKEYUP, (IntPtr)D, IntPtr.Zero);
+            }
+        }
+        public void shot() {
+            IntPtr WindowToFind = FindWindow(null, windowName);
+            Thread.Sleep(1000);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+           // System.Timers.Timer aTimer = new System.Timers.Timer();
+           // System.Windows.Forms.Timer timer;
+
+            actual = "STARTED";
+            stopwatch.Start();
+
+            aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            aTimer.Interval = 5000;
+            aTimer.Enabled = true;
+             
+            timer = new System.Windows.Forms.Timer();
+            timer.Enabled = true;
+            timer.Interval = 1000;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+            button1.Enabled = false;
+            button2.Enabled = true;
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            counter++;
+            label1.Text = "Cycle: " + counter.ToString();
+            label5.Text = "Elapsed: " + stopwatch.Elapsed.ToString();
+            if(actual != null)
+                label6.Text = actual.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            actual = "STOPPED";
+            aTimer.Enabled = false;
+            timer.Stop();
+            timer.Enabled = false;
+            button1.Enabled = true;
+            button2.Enabled = false;
+            timer.Dispose();
+            aTimer.Dispose();
+            counter = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bool isElevated;
+            using (WindowsIdentity identity = WindowsIdentity.GetCurrent())
+            {
+                WindowsPrincipal principal = new WindowsPrincipal(identity);
+                isElevated = principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+            if (isElevated == false)
+                //MessageBox.Show("Please run with Administrator rights!");
+            // NEMITAS A CSIPOGAS ELLEN
+            // SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle, (IntPtr)APPCOMMAND_VOLUME_MUTE);
+            stopwatch.Stop();
+            button2.Enabled = false;
+            // DEFAULT SPELLQUEUE
+            textBox3.Text = "-RRR-12E-3QR-4T-G0RX87";
+           // textBox3.Text = "-01201201201245677889";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            changeName(findHwnd(textBox1.Text), textBox2.Text);
+
+            IntPtr findHwnd(String wName) {
+            IntPtr hWnd = IntPtr.Zero;
+            foreach (Process pList in Process.GetProcesses())
+            {
+                if (pList.MainWindowTitle.Contains(wName))
+                {
+                    hWnd = pList.MainWindowHandle;
+                }
+            }
+            return hWnd;
+            }
+        }
+        void changeName(IntPtr handle, string name)
+        {
+            SetWindowText(handle, name);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+           // hack();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            windowName = textBox2.Text;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            forogjon = checkBox1.Checked;
+        }
+    }
+
+   
+
+}
